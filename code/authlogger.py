5d1781e956f90629106556db3600cf1cd3a300de
TylersDurden <srobbin1@ithaca.edu>
2019-12-01 19:30:19 -0500

    typo fix

--- code/authlogger.py
+++ code/authlogger.py
@@ -6,11 +6,17 @@ limit = 25
 os.system('sh badlogins.sh >> badlogins.txt')
 logindata = utils.swap('badlogins.txt', True)
 for line in logindata:
-    data = set(line.split(' '))
+    data = line.split(' ')
+    data = set(data)
+    data.remove('')
     try:
-        data.remove('')
-        count = int(data[0])
-        addr = data[1]
+        if len(data[0].split('.'))> 2:
+            count = data[1]
+            addr = data[0]
+        else:
+            count = data[0]
+            addr = data[1]
+
         if count >= limit:
             print '%s has attempted  %d logins ' % (addr, count)
     except:
