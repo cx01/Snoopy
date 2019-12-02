e70c8b55e2bb598a5889451617acca5e55797502
TylersDurden <srobbin1@ithaca.edu>
2019-12-01 19:21:02 -0500

    practical use

--- code/authlogger.py
+++ code/authlogger.py
@@ -1,27 +1,18 @@
-from threading import Thread
+import utils
 import time
 import os
 
-def swap(file_name, destroy):
-    data = []
-    for line in open(file_name,'r').readlines():
-        data.append(line.replace('\n', ''))
-    if destroy:
-        os.remove(file_name)
-    return data
-
-
-cmd = 'echo $(grep "Failed password for" /var/log/auth.log | grep -Po "[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+" ' \
-      '| sort | uniq -c)'
-os.system(cmd+' >> login_counter.txt')
-ip_counts = {}
-for line in swap('login_counter.txt', False):
+limit = 25
+os.system('sh security.sh >> badlogins.txt')
+logindata = utils.swap('badlogins.txt', True)
+for line in logindata:
     data = set(line.split(' '))
     try:
         data.remove('')
-    except KeyError:
-        continue
-    count = int(data[0])
-    addr = int(data[1])
-    if count > 30:
-        print '%s has attempted to login %d time. Adding to BlockList' % (addr, count)
+        count = int(data[0])
+        addr = data[1]
+    except:
+        pass
+    if count >= limit:
+        print '%s has attempted  %d logins ' % (addr, count)
+        # TODO: blacklist ip
