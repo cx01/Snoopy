04da8dd272e565a15897728d6fb1c8e0be44d04e
TylersDurden <srobbin1@ithaca.edu>
2019-12-06 20:51:20 -0500

    monitoring IPs attempting unauthorized SSH logins

--- code/authlogger.py
+++ code/authlogger.py
@@ -1,5 +1,6 @@
 import utils
 import time
+import sys
 import os
 
 tic = time.time()
@@ -34,9 +35,47 @@ def check_logs(limit, drop):
             print '* %d Connection Attempts made by %s' % (count, addr)
             if drop:
                 add_drop_rule(addr)
-    print '\033[1m Blocking These  \033[31m%d\033[0m\033[1m IP Adresses\033[0m' % len(blocked)
+    print '\033[1m Blocking These  \033[31m%d\033[1m IP Adresses\033[0m' % len(blocked)
     return blocked, data_out
 
 
-blocked_ips, attempt_counts = check_logs(limit=25, drop=False)
-print '\033[1mFINISHED\033[35m[%ss Elapsed] \033[0m' % str(time.time()-tic)
+def unique_attempts():
+    seen = []
+    counts = {}
+    os.system('sh badlogins.sh >> jerks.txt')
+    for line in utils.swap('jerks.txt', True):
+        data = set(line.split(' '))
+        data.pop()
+        a = data.pop()
+        b = data.pop()
+        if len(a.split('.')) >= 3:
+            addr = a
+            count = int(b)
+        else:
+            count = int(a)
+            addr = b
+        counts[addr] = count
+        seen.append(addr)
+    return seen, counts
+
+
+def dump_unique_addrs(file_out, ip_counts):
+    data = ''
+    for addr in ip_counts.keys():
+        count = ip_counts[addr]
+        data += '%s : %d\n' % (addr, count)
+    open(file_out, 'w').write(data)
+    return True
+
+
+if 'block' in sys.argv:
+    limit = 25
+    if '-limit' in sys.argv:
+        limit = int(sys.argv[4])
+    blocked_ips, attempt_counts = check_logs(limit=25, drop=True)
+    print '\033[1mFINISHED\033[32m [%ss Elapsed] \033[0m' % str(time.time() - tic)
+
+
+if 'dump' in sys.argv:
+    ip_list, counts = unique_attempts()
+    print '[*] %d Unique IP Addresses have Attempted SSH Logins as root' % len(ip_list)
